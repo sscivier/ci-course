8695c51f6a1344466bd57b332081514b0d6dfe0d
Sam Scivier <samscivier@gmail.com>
2022-10-18 10:33:32 +0100

    Add testing for invalid input to minimum()

--- ci_course/tests/test_functionality.py
+++ ci_course/tests/test_functionality.py
@@ -14,6 +14,10 @@ class TestFunctionality(unittest.TestCase):
         self.assertEqual(ci_course.minimum(1.2, 2.3), 1.2)
         self.assertEqual(ci_course.minimum(-1.2, -3), -3)
 
+    def test_minimum_none(self):
+        self.assertEqual(ci_course.minimum(1, '2', 3), None)
+        self.assertEqual(ci_course.minimum('My', 'Name', 'Is'), None)
+
 
 if __name__ == '__main__':
     unittest.main()
